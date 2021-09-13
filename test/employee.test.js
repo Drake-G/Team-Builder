af2879a86dfdb7a3a848ffaa7c777babb43db25d
Drake Gooding <dgooding1008@gmail.com>
2021-09-13 09:25:02 -0500

    First attempt at employee test

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,1 +1,10 @@
 const employee = require('../lib/employee');
+
+describe("employee", () => {
+    it("Should have an employee id, email, and name", () => {
+        const Employee = new employee("1",  "bob@bob.com", "Bob")
+        expect(employee.id).toEqual("1")
+        expect(employee.email).toEqual("bob@bob.com")
+        expect(employee.name).toEqual("Bob")
+    })
+})
